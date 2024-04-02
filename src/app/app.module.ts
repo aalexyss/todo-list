627dcc378258cc01848c11528cdc133562a9b066
Alexa Ezekieva <aleksa.ezekieva@kuru.it>
2024-04-02 15:13:19 +0300

    All files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskItemComponent } from './task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskItemComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/task-item/task-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss']
+})
+export class TaskItemComponent {
+  @Input() taskName: string | undefined;
+  @Input() assignee: string | undefined
+}
